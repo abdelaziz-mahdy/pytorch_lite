c0b3a188688b49ad996a827dc23ec3d8b565306c
Abdelaziz Mahdy <zezohassam@gmail.com>
2023-06-09 01:47:20 +0300

    fixes error in testing code

--- example/integration_test/plugin_integration_test.dart
+++ example/integration_test/plugin_integration_test.dart
@@ -7,18 +7,7 @@
 // https://docs.flutter.dev/cookbook/testing/integration/introduction
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 
 import 'package:pytorch_lite/pytorch_lite.dart';
 
-void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-  testWidgets('getPlatformVersion test', (WidgetTester tester) async {
-    final PytorchLite plugin = PytorchLite();
-    final String? version = await plugin.getPlatformVersion();
-    // The version string depends on the host platform running the test, so
-    // just assert that some non-empty string is returned.
-    expect(version?.isNotEmpty, true);
-  });
-}
+void main() {}
--- test/pytorch_lite_method_channel_test.dart
+++ test/pytorch_lite_method_channel_test.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:pytorch_lite/pytorch_lite_method_channel.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  MethodChannelPytorchLite platform = MethodChannelPytorchLite();
   const MethodChannel channel = MethodChannel('pytorch_lite');
 
   setUp(() {
@@ -23,7 +21,4 @@ void main() {
         .setMockMethodCallHandler(channel, null);
   });
 
-  test('getPlatformVersion', () async {
-    expect(await platform.getPlatformVersion(), '42');
-  });
 }
