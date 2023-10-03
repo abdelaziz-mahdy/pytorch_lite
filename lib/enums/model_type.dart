5e46ff4919d18dd8ed82f926a7fd92554f115713
Abdelaziz Mahdy <zezohassam@gmail.com>
2023-10-03 22:22:47 +0300

    added docstrings

--- lib/enums/model_type.dart
+++ lib/enums/model_type.dart
@@ -3,10 +3,18 @@ enum ObjectDetectionModelType {
   yolov8,
 }
 enum PreProcessingMethod {
+  /// Uses the imageLib library for preprocessing.
+  /// This method is less performant but ensures the same results on Android and iOS.
   imageLib,
+
+  /// Uses the native method for preprocessing.
+  /// This method is faster but may result in small changes in the preprocessing results.
   native,
 }
 enum CameraPreProcessingMethod {
+  /// Handles Android and iOS correctly.
   imageLib,
+
+  /// Only tested on Android.
   byteList,
 }
